d24de8031ece39a7bee75b68dd8bdf46297465a2
Chaitanya Upadhyay <chaitanyaupadhyay95@gmail.com>
2024-07-14 22:16:41 +0530

    refactored code

--- src/main/java/com/spring/employee/springRestEmployee/SpringRestEmployeeApplication.java
+++ src/main/java/com/spring/employee/springRestEmployee/SpringRestEmployeeApplication.java
@@ -10,12 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringRestEmployeeApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRestEmployeeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestEmployeeApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World");
+    }
 }
